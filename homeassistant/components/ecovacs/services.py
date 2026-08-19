f465f102f30bfa01f02830c5a806852461d9fa43
monsivar <32893134+monsivar@users.noreply.github.com>
2026-08-19 22:44:13 +0200

    Add lawn mower service to Ecovacs component
    Register set area parameter service

--- homeassistant/components/ecovacs/services.py
+++ homeassistant/components/ecovacs/services.py
@@ -1,12 +1,22 @@
 """Ecovacs services."""
 
+import voluptuous as vol
+
+from homeassistant.components.lawn_mower import DOMAIN as LAWN_MOWER_DOMAIN
 from homeassistant.components.vacuum import DOMAIN as VACUUM_DOMAIN
 from homeassistant.core import HomeAssistant, SupportsResponse, callback
-from homeassistant.helpers import service
+from homeassistant.helpers import config_validation as cv, service
 
 from .const import DOMAIN
 
 SERVICE_RAW_GET_POSITIONS = "raw_get_positions"
+SERVICE_SET_AREA_PARAMETER = "set_area_parameter"
+
+ATTR_AREA_ID = "area_id"
+ATTR_MOW_HEIGHT_LEVEL = "mow_height_level"
+ATTR_CUT_MODE = "cut_mode"
+ATTR_OBSTACLE_HEIGHT = "obstacle_height"
+ATTR_ANGLE = "angle"
 
 
 @callback
@@ -22,4 +32,20 @@ def async_setup_services(hass: HomeAssistant) -> None:
         schema=None,
         func="async_raw_get_positions",
         supports_response=SupportsResponse.ONLY,
+    )
+
+    # Lawn mower services
+    service.async_register_platform_entity_service(
+        hass,
+        DOMAIN,
+        SERVICE_SET_AREA_PARAMETER,
+        entity_domain=LAWN_MOWER_DOMAIN,
+        schema={
+            vol.Required(ATTR_AREA_ID): cv.string,
+            vol.Required(ATTR_MOW_HEIGHT_LEVEL): cv.positive_int,
+            vol.Required(ATTR_CUT_MODE): cv.positive_int,
+            vol.Required(ATTR_OBSTACLE_HEIGHT): cv.positive_int,
+            vol.Required(ATTR_ANGLE): cv.positive_int,
+        },
+        func="async_set_area_parameter",
     )
